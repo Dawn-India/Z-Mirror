a573712fcb54d112790148b7d2e8fac777d3db6e
Dawn India <61337422+shuvam-dawn@users.noreply.github.com>
2022-06-07 18:40:57 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -164,7 +164,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Z-Mirror-Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -434,7 +434,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Z-Mirror-Bot",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
