74ec092263f4520ab028e706b6869ba871e3e7e2
Dawn India <61337422+shuvam-dawn@users.noreply.github.com>
2022-06-18 18:00:10 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,10 +65,12 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Orginal Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
-    buttons.buildbutton("My Repo", "https://github.com/shuvam-dawn/MLTB-Custom-UI")
+    
+    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
+    buttons.buildbutton("Repo", "https://github.com/shuvam-dawn/MLTB-Custom-UI")
     buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
+    buttons.buildbutton("Owner", "https://t.me/dawn_in")
+    
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
