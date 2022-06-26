d68af71c493b251c175b20330f070f4b3eb2fe09
Dawn India <shuvam.dawn12345@gmail.com>
2022-06-27 02:10:24 +0530

    prev-comt-fix

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -7,7 +7,7 @@ from threading import RLock
 
 from bot import DOWNLOAD_DIR, AS_DOCUMENT, AS_DOC_USERS, AS_MEDIA_USERS, CUSTOM_FILENAME, \
                  EXTENSION_FILTER, app, LEECH_LOG, BOT_PM
-from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_video_resolution, get_path_size
+from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_path_size
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
 
 LOGGER = getLogger(__name__)
