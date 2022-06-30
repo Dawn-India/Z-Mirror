3d0da926a37da1a7f2f2577d62058606a1625c85
Dawn India <shuvam.dawn12345@gmail.com>
2022-06-30 16:57:04 +0530

    Minor Fix
    Issue - When BOT_PM is True and LEECH_LOG is empty and Chat type is Private bot was sending leeched files twice first time for current chat upload and second time for BOT_PM
    Fix - Now When LEECH_LOG is left empty and BOT_PM is True and if you using bot in PM, then bot will not send leeched files twice
    
    Co-Authored-By: Arsh Sisodiya <arshsisodiya@gmail.com>

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -17,7 +17,6 @@ VIDEO_SUFFIXES = ("MKV", "MP4", "MOV", "WMV", "3GP", "MPG", "WEBM", "AVI", "FLV"
 AUDIO_SUFFIXES = ("MP3", "M4A", "M4B", "FLAC", "WAV", "AIF", "OGG", "AAC", "DTS", "MID", "AMR", "MKA")
 IMAGE_SUFFIXES = ("JPG", "JPX", "PNG", "WEBP", "CR2", "TIF", "BMP", "JXR", "PSD", "ICO", "HEIC", "JPEG")
 
-
 class TgUploader:
 
     def __init__(self, name=None, listener=None):
@@ -40,6 +39,7 @@ class TgUploader:
         self.__leech_log = LEECH_LOG.copy()  # copy then pop to keep the original var as it is
         self.__app = app
         self.__user_id = listener.message.from_user.id
+        self.isPrivate = listener.message.chat.type in ['private', 'group']
     def upload(self):
         path = f"{DOWNLOAD_DIR}{self.__listener.uid}"
         size = get_readable_file_size(get_path_size(path))
@@ -126,7 +126,7 @@ class TgUploader:
                                                                       supports_streaming=True,
                                                                       disable_notification=True,
                                                                       progress=self.__upload_progress)
-                        if BOT_PM:
+                        if not self.isPrivate and BOT_PM:
                             try:
                                 app.send_video(chat_id=self.__user_id, video=self.__sent_msg.video.file_id,
                                                caption=cap_mono)
@@ -160,7 +160,7 @@ class TgUploader:
                                                                       thumb=thumb,
                                                                       disable_notification=True,
                                                                       progress=self.__upload_progress)
-                        if BOT_PM:
+                        if not self.isPrivate and BOT_PM:
                             try:
                                 app.send_audio(chat_id=self.__user_id, audio=self.__sent_msg.audio.file_id,
                                                caption=cap_mono)
@@ -186,7 +186,7 @@ class TgUploader:
                                                                       caption=cap_mono,
                                                                       disable_notification=True,
                                                                       progress=self.__upload_progress)
-                        if BOT_PM:
+                        if not self.isPrivate and BOT_PM:
                             try:
                                 app.send_photo(chat_id=self.__user_id, photo=self.__sent_msg.photo.file_id,
                                                caption=cap_mono)
@@ -221,7 +221,7 @@ class TgUploader:
                                                                      caption=cap_mono,
                                                                      disable_notification=True,
                                                                      progress=self.__upload_progress)
-                    if BOT_PM:
+                    if not self.isPrivate and BOT_PM:
                         try:
                             app.send_document(chat_id=self.__user_id, document=self.__sent_msg.document.file_id,
                                               caption=cap_mono)
