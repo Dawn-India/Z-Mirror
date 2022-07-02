31bcf6a3c1bc5e32c94793720765970cc2e67a4e
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-02 17:22:20 +0530

    Test fix hubdrive

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -542,16 +542,26 @@ def udrive(url: str) -> str:
         res = client.post(req_url, headers=headers, data=data).json()['file']
     except: return {'error': True, 'src_url': url}
 
-    if 'drivefire' in url:
+    if 'hubdrive' in url:
+        match = re_findall(r'https?://hubdrive\.(.+)\/\S+', url)[0]
+
+        with rsession() as client:
+            client.cookies.update({'crypt': HUBDRIVE_CRYPT})
+            client.get(url)
+            res = client.get(f"https://{match[0]}.hubdrive.{match[1]}/dl?id={url.split('/')[-1]}")
+        matches = re_findall('gd=(.*?)', res.text)
+        try:
+            decoded_id = b64decode(str(matches[0])).decode('utf-8')
+        except:
+            raise DirectDownloadLinkException("ERROR: Try in your broswer, mostly file not found or user limit exceeded!")
+        flink = f'https://drive.google.com/open?id={decoded_id}'
+        return flink
+    elif 'drivefire' in url:
         decoded_id = res.rsplit('/', 1)[-1]
         flink = f"https://drive.google.com/file/d/{decoded_id}"
         return flink
     elif 'drivehub' in url:
         gd_id = res.rsplit("=", 1)[-1]
-        flink = f"https://drive.google.com/open?id={gd_id}"
-        return flink
-    elif 'hubdrive' in url:
-        gd_id = res.rsplit("gd=", 1)[-1]
         flink = f"https://drive.google.com/open?id={gd_id}"
         return flink
     elif 'drivebuzz' in url:
