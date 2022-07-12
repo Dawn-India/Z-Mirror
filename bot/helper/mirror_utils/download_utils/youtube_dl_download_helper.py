524405134b82389a2e61f282fc584973e32e2646
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-13 02:44:04 +0530

    Update youtube_dl_download_helper.py

--- bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+++ bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
@@ -179,12 +179,12 @@ class YoutubeDLHelper:
                 msg = f'You must leave {STORAGE_THRESHOLD}GB free storage.'
                 msg += f'\nYour File/Folder size is {get_readable_file_size(self.size)}'
                 return self.__onDownloadError(msg)
-        if LEECH_LIMIT is not None:
-            acpt = check_storage_threshold(self.size, self.__listener.isLeech)
-            if not acpt:
-                msg = f'Leech Limit is: {LEECH_LIMIT}GB.'
-                msg += f'\nYour File/Folder size is {get_readable_file_size(self.size)}'
-                return self.__onDownloadError(msg)
+        # if LEECH_LIMIT is not None:
+        #     acpt = check_storage_threshold(self.size, self.__listener.isLeech)
+        #     if not acpt:
+        #         msg = f'Leech Limit is: {LEECH_LIMIT}GB.'
+        #         msg += f'\nYour File/Folder size is {get_readable_file_size(self.size)}'
+        #         return self.__onDownloadError(msg)
         if self.is_playlist:
             self.opts['outtmpl'] = f"{path}/{self.name}/%(title)s.%(ext)s"
         elif args is None:
