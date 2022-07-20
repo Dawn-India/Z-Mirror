cf7b3f32480ea72329ecf4f2f1ef318663ef324c
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-21 01:07:46 +0530

    Custom name for gdrive links
    Co-Authored-By: Anas <77075674+anasty17@users.noreply.github.com>

--- bot/helper/mirror_utils/download_utils/gd_downloader.py
+++ bot/helper/mirror_utils/download_utils/gd_downloader.py
@@ -7,10 +7,12 @@ from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMess
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
 from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold
 
-def add_gd_download(link, listener, is_gdtot):
+def add_gd_download(link, listener, newname, is_gdtot):
     res, size, name, files = GoogleDriveHelper().helper(link)
     if res != "":
         return sendMessage(res, listener.bot, listener.message)
+    if newname:
+        name = newname
     if STOP_DUPLICATE and not listener.isLeech:
         LOGGER.info('Checking File/Folder if already in Drive...')
         if listener.isZip:
--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -479,7 +479,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
             gmsg += f"Use /{BotCommands.UnzipMirrorCommand} to extracts Google Drive archive file"
             sendMessage(gmsg, bot, message)
         else:
-            Thread(target=add_gd_download, args=(link, listener, is_gdtot)).start()
+            Thread(target=add_gd_download, args=(link, listener, name, is_gdtot)).start()
     elif is_mega_link(link):
         if MEGA_KEY is not None:
             Thread(target=MegaDownloader(listener).add_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}/')).start()
