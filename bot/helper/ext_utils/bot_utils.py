5b66edf0835ec447eb8cb2333df259217b3dacc9
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-27 03:32:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -8,7 +8,6 @@ from urllib.request import urlopen
 from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
-from bot.helper.telegram_helper.message_utils import delete_all_messages, update_all_messages
 import shutil
 import psutil
 from psutil import virtual_memory, cpu_percent, disk_usage
@@ -18,6 +17,7 @@ from telegram.ext import CallbackQueryHandler
 from telegram.message import Message
 from telegram.update import Update
 from bot import *
+from bot.helper.telegram_helper.message_utils import delete_all_messages, update_all_messages
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
 
