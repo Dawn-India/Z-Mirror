8babde84060ab3b323add3b6a049fee7901995de
Dawn India <shuvam.dawn12345@gmail.com>
2022-08-09 00:27:22 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -121,7 +121,7 @@ def get_progress_bar_string(status):
     cFull = p // 8
     p_str = '⬢' * cFull
     p_str += '⬡' * (12 - cFull)
-    p_str = f"〘{p_str}〙"
+    p_str = f"{p_str}"
     return p_str
 
 def progress_bar(percentage):
