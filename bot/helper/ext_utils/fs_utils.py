be9e1436a1548649229b364481dba3ec13c2ec78
Dawn India <shuvam.dawn12345@gmail.com>
2022-08-18 00:06:29 +0530

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -134,60 +134,32 @@ def split_file(path, size, file_, dirpath, split_size, listener, start_time=0, i
         while i <= parts:
             parted_name = f"{str(base_name)}.part{str(i).zfill(3)}{str(extension)}"
             out_path = ospath.join(dirpath, parted_name)
-            listener.suproc = (
-                Popen(
-                    [
-                        "ffmpeg",
-                        "-hide_banner",
-                        "-loglevel",
-                        "error",
-                        "-ss",
-                        str(start_time),
-                        "-i",
-                        path,
-                        "-fs",
-                        str(split_size),
-                        "-map_chapters",
-                        "-1",
-                        "-c",
-                        "copy",
-                        out_path,
-                    ]
-                )
-                if noMap
-                else Popen(
-                    [
-                        "ffmpeg",
-                        "-hide_banner",
-                        "-loglevel",
-                        "error",
-                        "-ss",
-                        str(start_time),
-                        "-i",
-                        path,
-                        "-fs",
-                        str(split_size),
-                        "-map",
-                        "0",
-                        "-map_chapters",
-                        "-1",
-                        "-c",
-                        "copy",
-                        out_path,
-                    ]
-                )
-            )
+            if not noMap:
+                listener.suproc = Popen(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
+                                         "-i", path, "-fs", str(split_size), "-map", "0", "-map_chapters", "-1",
+                                         "-c", "copy", out_path])
+            else:
+                listener.suproc = Popen(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
+                                          "-i", path, "-fs", str(split_size), "-map_chapters", "-1", "-c", "copy",
+                                          out_path])
 
             listener.suproc.wait()
             if listener.suproc.returncode == -9:
                 return False
             elif listener.suproc.returncode != 0 and not noMap:
-                LOGGER.warning(f'Retrying without map, -map 0 not working in all situations. Path: {path}')
+                LOGGER.warning(f"Retrying without map, -map 0 not working in all situations. Path: {path}")
                 try:
                     osremove(out_path)
                 except:
                     pass
                 return split_file(path, size, file_, dirpath, split_size, listener, start_time, i, True, True)
+            elif listener.suproc.returncode != 0:
+                LOGGER.warning(f"Unable to split this video, if it's size less than {MAX_SPLIT_SIZE} will be uploaded as it is. Path: {path}")
+                try:
+                    osremove(out_path)
+                except:
+                    pass
+                return "errored"
             out_size = get_path_size(out_path)
             if out_size > (TG_SPLIT_SIZE + 1000):
                 dif = out_size - (TG_SPLIT_SIZE + 1000)
@@ -218,7 +190,7 @@ def get_media_info(path):
 
     try:
         result = check_output(["ffprobe", "-hide_banner", "-loglevel", "error", "-print_format",
-                               "json", "-show_format", path]).decode('utf-8')
+                               "json", "-show_format", "-show_streams", path]).decode('utf-8')
     except Exception as e:
         LOGGER.error(f'{e}. Mostly file not found!')
         return 0, None, None
@@ -242,3 +214,31 @@ def get_media_info(path):
         artist = None
 
     return duration, artist, title
+
+def get_media_streams(path):
+
+    is_video = False
+    is_audio = False
+
+    mime_type = get_mime_type(path)
+    if not mime_type.startswith(('video', 'audio')):
+        return is_video, is_audio
+
+    try:
+        result = check_output(["ffprobe", "-hide_banner", "-loglevel", "error", "-print_format",
+                               "json", "-show_streams", path]).decode('utf-8')
+    except Exception as e:
+        LOGGER.error(f'{e}. Mostly file not found!')
+        return is_video, is_audio
+
+    fields = jsnloads(result).get('streams')
+    if fields is None:
+        LOGGER.error(f"get_media_streams: {result}")
+        return is_video, is_audio
+
+    for stream in fields:
+        if stream.get('codec_type') == 'video':
+            is_video = True
+        elif stream.get('codec_type') == 'audio':
+            is_audio = True
+    return is_video, is_audio
