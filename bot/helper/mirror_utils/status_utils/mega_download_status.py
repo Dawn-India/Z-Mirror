9ac12f6d56f15b7b7a46282f54e3939d2346aa38
Dawn India <shuvam.dawn12345@gmail.com>
2022-08-23 18:54:14 +0530

    Added bot stats, engine stats, split stats & z-ui

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,5 +1,4 @@
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time
-
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time, EngineStatus
 
 class MegaDownloadStatus:
 
@@ -54,3 +53,6 @@ class MegaDownloadStatus:
 
     def download(self):
         return self.__obj
+
+    def eng(self):
+        return EngineStatus.STATUS_MEGA
