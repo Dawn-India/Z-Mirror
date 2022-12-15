f75ec257be8c60ded9428d3d76a8039d2bfd1979
Angel Noor <113758454+Angel-noor@users.noreply.github.com>
2022-12-15 07:26:21 -0800

    FilePress Define

--- bot/helper/mirror_utils/download_utils/gd_downloader.py
+++ bot/helper/mirror_utils/download_utils/gd_downloader.py
@@ -7,7 +7,7 @@ from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMess
 from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
 
-def add_gd_download(link, path, listener, newname):
+def add_gd_download(link, path, listener, newname, is_filepress):
     res, size, name, files = GoogleDriveHelper().helper(link)
     if res != "":
         return sendMessage(res, listener.bot, listener.message)
