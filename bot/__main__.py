c73c27046e394cbea0cb9385c5a326083abf9802
Arun <55507529+sudoarun@users.noreply.github.com>
2023-01-03 22:54:06 +0530

    sds

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,10 +56,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Admin", "https://t.me/sudoarun")
+    buttons.buildbutton("Mirro Group", "https://t.me/+meHt4ljT3IQzZDk1")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
