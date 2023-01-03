874ee9d0db6792a3cf0e4c057c3fc685bca8c95a
Arun <55507529+sudoarun@users.noreply.github.com>
2023-01-03 23:11:34 +0530

    xvz

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ T: {disk_t} | F: {disk_f}
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
 
-Made with ❤️ by Dawn
+Made with ❤️ by Sudoarun
 """
 
 #---Thanks for deleting my name ❤️ Appreciate it---#
