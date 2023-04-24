0ac59f2a2fd6da381476c776f34bced3e06a2f56
Dawn India <dawn-in@z-mirror.live>
2023-04-25 02:20:08 +0530

    Update gd_count.py

--- bot/modules/gd_count.py
+++ bot/modules/gd_count.py
@@ -3,7 +3,7 @@ from time import time
 from pyrogram.filters import command
 from pyrogram.handlers import MessageHandler
 
-from bot import bot
+from bot import bot, config_dict
 from bot.helper.ext_utils.bot_utils import (get_readable_file_size,
                                             get_readable_time, is_gdrive_link,
                                             new_task, sync_to_async)
