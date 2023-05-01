315d1ef4f5c9ac3e9c842062c270ca9240ba1695
Dawn India <dawn-in@z-mirror.live>
2023-05-01 12:36:39 +0530

    Update __init__.py
    fix torrent select

--- bot/__init__.py
+++ bot/__init__.py
@@ -523,9 +523,8 @@ if ospath.exists('categories.txt'):
                 tempdict['index_link'] = ''
             categories_dict[name] = tempdict
 
-if BASE_URL:
-    Popen(
-        f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent", shell=True)
+PORT = environ.get('PORT')
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
 
 info("Starting qBittorrent-Nox")
 zrun(["qbittorrent-nox", "-d", "--profile=."])
